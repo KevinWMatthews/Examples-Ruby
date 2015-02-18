5a7eb5921d920c8f7eead0e73939e1a2ee2ca9c3
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-18 07:57:28 -0800

    Misc formatting stuff

---
+++ PoundBraceThing.rb
@@ -0,0 +1,12 @@
+#Nifty
+puts "  \#{} Demo"
+puts
+
+puts "\#{1 + 2 + 3}:"
+puts "#{1 + 2 + 3}"
+puts
+
+puts "Use '' to force Ruby to ignore
+the operation or variable within \#{}:"
+puts "#{'1 + 2 + 3'}"
+puts
---
+++ StringFormatting.rb
@@ -0,0 +1,14 @@
+formatter = "%{first} %{second} %{third} %{fourth}"
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+
+puts """
+Whoa!
+With three quotes you can type
+as
+much
+as
+you
+like!!
+"""
