756d4e55d4b6bb2ca242b848a4a491591c4a6506
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-25 16:45:02 -0800

    Random number generation with rand

---
+++ Random/Random.rb
@@ -0,0 +1,9 @@
+#Generating random numbers in ruby
+
+#rand(<range>)
+
+puts "Generate random numbers between 0 and 4:"
+10.times {puts rand(5)}
+
+puts "Generate random numbers between 0 and 999:"
+10.times {puts rand(1000)}
