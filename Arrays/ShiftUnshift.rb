a59359918c6f18d42b0297ce56c698b6d514b97b
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-25 16:44:35 -0800

    Document ShiftUnshift

--- Arrays/ShiftUnshift.rb
+++ Arrays/ShiftUnshift.rb
@@ -4,22 +4,22 @@ puts "Initialize a:"
 puts a.inspect
 puts
 
-puts "Shift:"
+puts "Shift removes the first element:"
 puts a.shift
 puts a.inspect
 puts
 
-puts "Unshift:"
+puts "Unshift adds to the beginning of the array:"
 puts a.unshift(42)
 puts a.inspect
 puts
 
-puts "Push:"
+puts "Push adds to the end of the array:"
 puts a.push(66)
 puts a.inspect
 puts
 
-puts "Pop:"
+puts "Pop removes from the end of the array:"
 puts a.pop
 puts a.inspect
 puts
